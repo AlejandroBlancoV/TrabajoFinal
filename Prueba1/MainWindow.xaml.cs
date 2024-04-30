8474042acf6a267697b72458aaab416e01e33d24
Alejandro Blanco <alejandroblancovillar2004@gmail.com>
2024-04-30 16:13:25 +0200

    Agregar archivos de proyecto.

---
+++ Prueba1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prueba1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Obtener el tamaño de la pantalla
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            // Establecer el tamaño de la ventana
+            Width = screenWidth;
+            Height = screenHeight;
+        }
+    }
+}
